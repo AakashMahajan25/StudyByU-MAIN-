aff39620473f4d56b5ef6708e8bfdf801c0e1dd6
007Vatsal <vatsalbhakodia@gmail.com>
2024-05-14 11:51:53 +0530

    notes page playlist added

--- index.php
+++ index.php
@@ -3,12 +3,64 @@ session_start();
 include('./components/header.html');
 ?>
 
-    <div>
-        <div classname="section-1"></div>
-        <div classname="main-section">
+    <div class="flex">
+        <div class="section-1"></div>
+        <div class="main-section">
             <div class="pdf-viewer" id="pdf-viewer"></div>
         </div>
-        <div classname="section-3"></div>
+        <div class="section-3">
+
+            <div class="playlist">
+                <div class="playlist-header">
+                    <div class="chapter">Chapter-1</div>
+                    <div class="subject">Science <span>1/5</span></div>
+                </div>
+                <div class="playlist-body">
+
+                    <div class="video active">
+                        <div class="numbering">1</div>
+                        <div class="thumbnail">
+                            <img src="assets/dummy-thumbnail.jpg">
+                        </div>
+                        <div class="video-title">Video Title</div>
+                    </div>
+
+                    <div class="video">
+                        <div class="numbering">2</div>
+                        <div class="thumbnail">
+                            <img src="assets/dummy-thumbnail.jpg">
+                        </div>
+                        <div class="video-title">Video Title</div>
+                    </div>
+
+                    <div class="video">
+                        <div class="numbering">3</div>
+                        <div class="thumbnail">
+                            <img src="assets/dummy-thumbnail.jpg">
+                        </div>
+                        <div class="video-title">Video Title</div>
+                    </div>
+
+                    <div class="video">
+                        <div class="numbering">4</div>
+                        <div class="thumbnail">
+                            <img src="assets/dummy-thumbnail.jpg">
+                        </div>
+                        <div class="video-title">Video Title</div>
+                    </div>
+
+                    <div class="video">
+                        <div class="numbering">5</div>
+                        <div class="thumbnail">
+                            <img src="assets/dummy-thumbnail.jpg">
+                        </div>
+                        <div class="video-title">Video Title</div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
     </div>
 
 
--- javascript/pdf-viewer.js
+++ javascript/pdf-viewer.js
@@ -9,7 +9,7 @@ pdfjsLib.getDocument(pdfUrl).promise.then(function(pdf) {
 
             // Set canvas dimensions to match the PDF viewer div width and viewport height
             const viewerWidth = pdfViewer.offsetWidth;
-            const viewport = page.getViewport({ scale: 2 });
+            const viewport = page.getViewport({ scale: 1.7 });
             const viewerHeight = viewport.height * (viewerWidth / viewport.width); // Maintain aspect ratio
             canvas.width = viewerWidth;
             canvas.height = viewerHeight;
