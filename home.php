5b347306184135a7b08f0b67fcb2744a678b7004
aakash <mahajanaakash108@gmail.com>
2024-05-27 16:43:16 +0530

    A

--- home.php
+++ home.php
@@ -1,7 +1,8 @@
 <script src="https://cdn.tailwindcss.com"></script>
 <link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/home.css">
 
-<div class="bg-[url('./assets/home-banner-bg.png')] h-[100vh] w-[100vw] bg-cover flex justify-around">
+<div class="bg-[url('./assets/home-banner-bg.webp')] h-[100vh] w-[100vw] bg-cover flex justify-around">
     <div class="mt-32 flex flex-col gap-3">
         <div class="text-white font-black text-5xl">Embark on your educational</div>
         <div class="text-white font-black text-5xl">journey with us today.</div>
@@ -11,3 +12,107 @@
     </div>
     <div><img src="assets/home-hero-image.png" alt=""></div>
 </div>
+
+<!-- Four Element Below -->
+<div class="four-element-container">
+    <div class="our-offer">Our <span class="highlight">Offerings</span></div>
+    <div class="component">
+        <div class="courses">
+            <div class="course-card bg-[#FCE4EC]">
+                <h2>Class IX</h2>
+                <h3>All Subjects</h3>
+                <p>Join our comprehensive Class 9 course Today!</p>
+                <!-- <img style="width: 60px; height: auto;" src="assets/9.png" alt=""> -->
+                <p class="standard">9 </p>
+                <button>Explore</button>
+            </div>
+            <div class="course-card bg-[#CDDEFF]">
+                <h2>Class X</h2>
+                <h3>All Subjects</h3>
+                <p>Join our comprehensive Class 10 course Today!</p>
+                <!-- <img style="width: 70px; height: auto;" src="assets/10.png" alt=""> -->
+                <p class="standard">10</p>
+                <button>Explore</button>
+            </div>
+            <div class="course-card bg-[#FFF0CB]">
+                <h2>LIVE</h2>
+                <h3>Doubt Clearing Sessions</h3>
+                <p>Get personalized help in our doubt clearing sessions.</p>
+                <img style="width: 70px; height: auto;" src="assets/doubt.png" alt="">
+                <button>Explore</button>
+            </div>
+            <div class="course-card new bg-[#B6FFE4]">
+                <h2>LIVE</h2>
+                <h3>Mental Health Sessions</h3>
+                <p>Enhance well-being with our mental health sessions.</p>
+                <img style="width: 70px; height: auto;" src="assets/m-health.png" alt="">
+                <button>Explore</button>
+            </div>
+        </div>
+        <img src="assets/4-element-image.webp" alt="">
+        
+    </div>
+</div>
+
+
+<!-- Another Section -->
+<section class="bg-white ">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+        <div class="max-w-screen-md mb-8 lg:mb-16">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-[#6180f7] ">Tailored for learners like you,</h2>
+            <p class="text-gray-500 sm:text-xl ">Here at StudyByU, we focus on areas where technology, innovation, and knowledge can unlock long-term value and drive educational growth.</p>
+        </div>
+        <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+            <div class="bg-[#fde5ec] p-5 rounded-2xl">
+                <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12">
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                </div>
+                <h3 class="mb-2 text-xl font-bold">Course Catalog</h3>
+                <p class="text-gray-500">Explore, enroll, excel. Discover a wide range of courses tailored to meet your learning needs and help you achieve your academic goals with our comprehensive course catalog.</p>
+            </div>
+            <div class="bg-[#cddeff] p-5 rounded-2xl">
+                <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12">
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                </div>
+                <h3 class="mb-2 text-xl font-bold">Interactive Learning</h3>
+                <p class="text-gray-500">Engage, interact, succeed. Dive into an immersive learning experience with interactive lessons and real-time feedback designed to enhance your understanding and retention.</p>
+            </div>
+            <div class="bg-[#fef1cb] p-5 rounded-2xl">
+                <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12">
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                </div>
+                <h3 class="mb-2 text-xl font-bold">Personalized Pathways</h3>
+                <p class="text-gray-500">Customize, progress, achieve. Tailor your learning journey with personalized pathways that align with your interests and career aspirations, ensuring you stay motivated and on track.</p>
+            </div>
+            <div class="bg-[#b7ffe5] p-5 rounded-2xl">
+                <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12">
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                </div>
+                <h3 class="mb-2 text-xl font-bold">Collaborative Projects</h3>
+                <p class="text-gray-500">Connect, collaborate, innovate. Work together with peers from around the world on group projects that foster teamwork, creativity, and practical problem-solving skills.</p>
+            </div>
+            <div class="bg-[#FFA6A0] p-5 rounded-2xl">
+                <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12">
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                </div>
+                <h3 class="mb-2 text-xl font-bold">Expert Instructors</h3>
+                <p class="text-gray-500">Learn from the best. Gain insights and knowledge from industry experts and experienced educators who are dedicated to helping you succeed in your studies.</p>
+            </div>
+            <div class="bg-[#FDFD96] p-5 rounded-2xl">
+                <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12">
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                </div>
+                <h3 class="mb-2 text-xl font-bold">Career Support</h3>
+                <p class="text-gray-500">Prepare, apply, succeed. Access a range of career support services, including resume building, interview preparation, and job placement assistance, to help you transition from learning to earning.
+
+
+
+
+
+
+                </p>
+            </div>
+            
+        </div>
+    </div>
+  </section>
