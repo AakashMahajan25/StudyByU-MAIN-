e114760e2db5c4362a745fd4ec80c09a7b9aa6da
007Vatsal <vatsalbhakodia@gmail.com>
2024-05-30 19:31:40 +0530

    dashboard final changes

--- dashboard-main.php
+++ dashboard-main.php
@@ -21,7 +21,6 @@ include('./components/header.html');
         width: 100%;
         height: 100%;
         background-color: aliceblue;
-        /* /* border: 4px solid #3b82f6;  */
     }
 
     .progress-wrapper::after {
@@ -30,39 +29,33 @@ include('./components/header.html');
         top: 10%;
         left: 10%;
         background-color: white;
-        /* Tailwind gray-200 color */
     }
 </style>
 
 
 <div class="lg:fixed lg:top-0 lg:right-0">
 
-    <div class="flex flex-wrap items-start gap-6 lg:w-[95vw] my-5 mx-6">
+    <div class="flex flex-wrap items-start gap-5 lg:w-[93vw] m-3">
         
         <!-- info -->
         <div class="lg:h-[45vh] lg:w-[40vw] w-full md:w-[53vw] bg-white p-5 rounded-xl shadow-lg">
-            <div class="flex justify-between">
-                <div class="text-2xl lg:text-5xl text-[#013266] font-bold mb-4">
-                    Hi Aakash!
+            <div class="flex flex-col justify-between h-full mx-5">
+                <div class="flex justify-between">
+                    <div class="text-xs">June 30, Thursday</div>
+                    <div class="text-gray-600"><i class="fa-solid fa-user-pen fa-md"></i></div>
                 </div>
-                <div class="text-gray-600"><i class="fa-solid fa-user-pen fa-lg"></i></div>
-            </div>
-            <div class="flex">
                 <img class="w-[15vw] lg:w-[10vw] rounded-full" src="./assets/user.png" alt="User Profile">
-                <div class="flex flex-col justify-center ml-5 overflow-hidden">
-                    <ul class="space-y-2 text-lg text-gray-700">
-                        <li class="text-sm lg:text-xl"><i class="fa-solid fa-envelope"></i> aakashmahajan2004@gmail.com
-                        </li>
-                        <li class="text-sm lg:text-xl"><i class="fa-solid fa-phone"></i> +91 9582799219</li>
-                    </ul>
+                <div>
+                    <div class="text-3xl font-semibold">Welcome back, User!</div>
+                    <div>Unlocking potential with every step.</div>
                 </div>
-            </div>
-        </div>
+            </div>    
 
+        </div>
 
         <!-- upcoming sessions -->
         <div class="w-full md:w-[38vw] lg:w-[30vw] lg:h-[45vh] border-1 border-indigo-500 px-5 py-4 rounded-xl bg-white shadow-lg ">
-            <div class="text-xl lg:text-3xl font-semibold text-[#013266] mb-4">Upcoming Sessions</div>
+            <div class="text-xl lg:text-2xl font-semibold mb-4">Upcoming Sessions</div>
             <div class="h-[30vh] space-y-4 overflow-y-auto">
                 <div class="bg-[aliceblue] py-2 px-4 rounded-lg shadow-sm ">
                     <div class="flex items-center justify-between">
@@ -137,120 +130,91 @@ include('./components/header.html');
         </div>
     
         <!-- DCS & MHS Cards -->
-    <div class="md:flex lg:flex-col md:justify-around w-full lg:w-[20vw] lg:h-[45vh] overflow-hidden">
-        <div
-            class="lg:w-[20vw] lg:h-[22vh] md:w-[40vw] bg-white p-4 border border-gray-200 rounded-lg shadow-lg flex flex-col justify-between gap-4 mb-2">
+        <div class="md:flex lg:flex-col md:justify-around w-full lg:w-[20vw] lg:h-[45vh] overflow-hidden">
+            <div class="lg:w-[20vw] lg:h-[22vh] md:w-[40vw] bg-white p-4 border border-gray-200 rounded-lg shadow-lg flex flex-col gap-3 mb-2">
+                <div class="text-xs lg:text-sm">DCS - StudyByU</div>
+                <div>
+                    <div class="text-xl lg:text-lg">Doubt Clearing Sessions</div>
+                    <div class="text-xs lg:text-sm">Clear All your Doubts!</div>
+                </div>
+                <div class="text-sm lg:text-md text-blue-500">
+                    BOOK NOW <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                </div>
+            </div>
+            <div class="lg:w-[20vw] lg:h-[22vh] md:w-[40vw] bg-white p-4 border border-gray-200 rounded-lg shadow-lg flex flex-col gap-3 mb-2">
+                <div class="text-xs lg:text-sm">MHC - StudyByU</div>
+                <div>
+                    <div class="text-xl lg:text-lg">Mental Health Advice</div>
+                    <div class="text-xs lg:text-sm">Support for a better you.</div>
+                </div>
+                <div class="text-sm lg:text-md text-blue-500">
+                    BOOK NOW <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                </div>
+            </div>
 
-            <div class="flex h-fit">
-                <div class="text-xs lg:text-sm">
-                    DCS
-                </div>
-                <div class="font-bold mx-4 text-xs lg:text-sm">·</div>
-                <div class="text-xs lg:text-sm">
-                    StudyByU
-                </div>
-            </div>
-            <div>
-                <div class="text-xl lg:text-xl">Doubt Clearing Sessions</div>
-                <div class="text-xs lg:text-sm">
-                    Clear All your Doubts!
-                </div>
-            </div>
-            <div class="text-sm lg:text-xl text-blue-500">
-                BOOK NOW <i class="fa-solid fa-arrow-up-right-from-square"></i>
-            </div>
-        </div>
-        <div
-            class="lg:w-[20vw] lg:h-[22vh] md:w-[40vw] bg-white p-4 border border-gray-200 rounded-lg shadow-lg flex flex-col justify-between gap-4 mb-2">
-
-            <div class="flex h-fit">
-                <div class="text-xs lg:text-sm">
-                    DCS
-                </div>
-                <div class="font-bold mx-4 text-xs lg:text-sm">·</div>
-                <div class="text-xs lg:text-sm">
-                    StudyByU
-                </div>
-            </div>
-            <div>
-                <div class="text-xl lg:text-xl">Doubt Clearing Sessions</div>
-                <div class="text-xs lg:text-sm">
-                    Clear All your Doubts!
-                </div>
-            </div>
-            <div class="text-sm lg:text-xl text-blue-500">
-                BOOK NOW <i class="fa-solid fa-arrow-up-right-from-square"></i>
-            </div>
-        </div>
         </div>
 
-
-
-
-    <!-- chart -->
-    <div class="w-full lg:w-[50vw] lg:h-[48vh] bg-white p-4 rounded-xl shadow-l lg:mb-0">
-        <div class="text-2xl md:text-3xl lg:text-4xl font-semibold text-[#013266]">Your Weekly Progress</div>
-        <div class="w-full lg:w-[45vw]">
-            <canvas id="myChart" width="100vh" height="40vh"></canvas>
-        </div>
-    </div>
-
-    <!-- number of sessions -->
-    <div class="md:flex lg:flex-col lg:h-[47vh] md:justify-around w-full lg:w-[20vw] mb-3">
-        <div class="bg-white md:w-[40vw] lg:w-[20vw] py-4 px-5 rounded-xl shadow-lg mt-5 mb-2 md:mt-0">
-            <div class="flex justify-between items-center h-[7vh]">
-                <div class="text-xl md:text-3xl lg:text-4xl text-blue-500">10+</div>
-                <i
-                    class="fa-solid fa-question flex items-center justify-center w-5 h-5 lg:w-12 lg:h-12 rounded-full bg-blue-100 text-blue-500 text-sm lg:text-3xl"></i>
-            </div>
-
-            <div class="mt-5">
-                <div class="text-xl md:text-2xl lg:text-2xl font-semibold">Doubt Clearing</div>
-                <div class="text-sm md:text-lg lg:text-lg">Total Sessions Taken</div>
+        <!-- chart -->
+        <div class="w-full lg:w-[50vw] lg:h-[47vh] bg-white p-4 rounded-xl shadow-lg">
+            <div class="text-2xl md:text-3xl lg:text-2xl font-semibold">Your Weekly Progress</div>
+            <div class="w-full lg:w-[45vw]">
+                <canvas id="myChart" width="100vh" height="40vh"></canvas>
             </div>
         </div>
 
-        <div class="bg-white py-4 px-5 md:w-[40vw] lg:w-[20vw] rounded-xl shadow-lg mt-2 mb-5 md:mt-0 md:mb-2">
-            <div class="flex justify-between items-center h-[7vh]">
-                <div class="text-xl md:text-3xl lg:text-4xl text-blue-500">20+</div>
-                <i
-                    class="fa-solid fa-heart flex items-center justify-center w-5 h-5 lg:w-12 lg:h-12 rounded-full bg-blue-100 text-blue-500 text-sm lg:text-xl"></i>
+        <!-- number of sessions -->
+        <div class="flex gap-4 lg:flex-col lg:h-[47vh] lg:w-[20vw] md:justify-between w-full">
+            <div class="h-1/2 bg-white py-4 px-5 md:w-[40vw] lg:w-[20vw] rounded-xl shadow-lg">
+                <div class="flex justify-between items-center h-[7vh]">
+                    <div class="text-xl md:text-3xl lg:text-4xl text-blue-500">10+</div>
+                    <i
+                        class="fa-solid fa-question flex items-center justify-center w-5 h-5 lg:w-12 lg:h-12 rounded-full bg-blue-100 text-blue-500 text-sm lg:text-3xl"></i>
+                </div>
+
+                <div class="mt-5">
+                    <div class="text-xl md:text-2xl lg:text-xl font-semibold">Doubt Clearing</div>
+                    <div class="text-sm md:text-lg lg:text-sm">Total Sessions Taken</div>
+                </div>
             </div>
 
-            <div class="mt-5">
-                <div class="text-xl md:text-2xl lg:text-2xl font-semibold">Mental Health</div>
-                <div class="text-sm md:text-lg lg:text-lg">Total Sessions Taken</div>
+            <div class="h-1/2 bg-white py-4 px-5 md:w-[40vw] lg:w-[20vw] rounded-xl shadow-lg">
+                <div class="flex justify-between items-center h-[7vh]">
+                    <div class="text-xl md:text-3xl lg:text-4xl text-blue-500">20+</div>
+                    <i
+                        class="fa-solid fa-heart flex items-center justify-center w-5 h-5 lg:w-12 lg:h-12 rounded-full bg-blue-100 text-blue-500 text-sm lg:text-xl"></i>
+                </div>
+
+                <div class="mt-5">
+                    <div class="text-xl md:text-2xl lg:text-xl font-semibold">Mental Health</div>
+                    <div class="text-sm md:text-lg lg:text-sm">Total Sessions Taken</div>
+                </div>
             </div>
         </div>
-    </div>
 
-    <!-- Daily Progress -->
-    <div class="w-full lg:w-[20vw] lg:h-[47vh] md:w-[60vw] p-6 bg-white border border-gray-200 rounded-lg shadow-lg mx-auto mb-3">
-        <div class="text-xl md:text-3xl lg:text-3xl font-semibold text-center mb-4">Daily Progress</div>
-        <div class="text-sm md:text-lg font-medium text-center mb-1">Keep improving the</div>
-        <div class="text-sm md:text-lg font-medium text-center mb-6">quality of your study</div>
+        <!-- Daily Progress -->
+        <div class="w-full lg:w-[20vw] lg:h-[47vh] md:w-[60vw] p-6 bg-white border border-gray-200 rounded-lg shadow-lg">
+            <div class="text-xl md:text-3xl lg:text-3xl font-semibold text-center">Daily Progress</div>
+            <div class="text-sm md:text-md text-center mt-2 mb-4">Keep improving the <br>quality of your study</div>
 
-        <div class="flex justify-center">
-            <div
-                class="relative h-20 w-20 md:w-30 md:h-30 lg:w-40 lg:h-40 flex items-center justify-center progress-wrapper">
-                <div class="absolute inset-0 flex items-center justify-center">
-                    <div class="relative w-full h-full flex items-center justify-center">
-                        <div class="absolute inset-0 flex items-center justify-center text-black">
-                            <div class="w-full h-full rounded-full overflow-hidden ">
-                                <div class="w-full h-full bg-blue-500 rounded-full"
-                                    style="clip-path: inset(0% 0% 0% 50%)">
+            <div class="flex justify-center">
+                <div class="relative h-20 w-20 md:w-30 md:h-30 lg:w-40 lg:h-40 flex items-center justify-center progress-wrapper">
+                    <div class="absolute inset-0 flex items-center justify-center">
+                        <div class="relative w-full h-full flex items-center justify-center">
+                            <div class="absolute inset-0 flex items-center justify-center text-black">
+                                <div class="w-full h-full rounded-full overflow-hidden ">
+                                    <div class="w-full h-full bg-blue-500 rounded-full"
+                                        style="clip-path: inset(0% 0% 0% 50%)">
+                                    </div>
                                 </div>
                             </div>
+                            <div class="absolute inset-0 flex items-center justify-center text-xl md:text-3xl lg:text-4xl font-semibold z-10">
+                                50%
+                            </div>
                         </div>
-                        <div
-                            class="absolute inset-0 flex items-center justify-center text-xl md:text-3xl lg:text-5xl font-bold z-10">
-                            50%</div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
-
-
-</div>
 </div>
