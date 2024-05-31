3a86bf704ed385d3407a8a699f97cace7f018fad
007Vatsal <vatsalbhakodia@gmail.com>
2024-05-31 18:16:43 +0530

    product page css changes

--- product.php
+++ product.php
@@ -22,7 +22,7 @@ include('./secondary-navbar.html');
       <div class="left">
         <div class="flex  w-full pt-10">
           <div class="w-1/3 px-10 ">
-            <img src="assets/1.webp" alt="" class="rounded-lg img" />
+            <img src="assets/4.webp" alt="" class="rounded-lg img" />
           </div> 
 
           <div class="  w-[60%] flex flex-col justify-around ">
@@ -31,14 +31,13 @@ include('./secondary-navbar.html');
                 <span class="  text-xl leading-none  other-ch block xl:mb-4">Chapter 4 </span>
                 Linear Equations in Two Variables
               </h1>
-              <div class="text-[1.6vw] font-semibold">Rs. 150 <del class="text-[1vw]">Rs. 200</del></div>
+              <div class="text-[1.6vw] font-semibold">₹48 <del class="text-[1vw] text-gray-500">50</del></div>
             </div>
   
             <div class="text-[1.15vw] text-gray-600">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae
               neque recusandae aliquid officiis dolores mollitia quod ipsam sunt
-              quo dolor laudantium vero, debitis iste aut? Tempore debitis
-              doloremque quasi minima.
+              quo dolor laudantium vero, debitis iste aut?
             </div>
             <div>
               <button class="buy-now-btn">Buy Now</button>
@@ -107,7 +106,7 @@ include('./secondary-navbar.html');
                               <img src="assets/2.webp" alt="" class="w-full rounded-lg" />
                           </div>
                           <h2 class="w-2/3 font-ch-name">
-                              <span class="block block text-sm other-ch">Chapter - 1</span>
+                              <span class="block text-xs text-gray-500">Chapter - 1</span>
                               Number Systems
                           </h2>
                           <div class="arrow-circle">></div>
@@ -117,7 +116,7 @@ include('./secondary-navbar.html');
                               <img src="assets/3.webp" alt="" class="w-full rounded-lg" />
                           </div>
                           <h2 class="w-2/3 font-ch-name">
-                              <span class="block text-sm other-ch">Chapter - 1</span>
+                              <span class="block text-xs text-gray-500">Chapter - 2</span>
                               Polynomials
                           </h2>
                           <div class="arrow-circle">></div>
@@ -127,7 +126,7 @@ include('./secondary-navbar.html');
                               <img src="assets/4.webp" alt="" class="w-full rounded-lg" />
                           </div>
                           <h2 class="w-2/3 font-ch-name">
-                              <span class="block text-sm other-ch">Chapter - 1</span>
+                              <span class="block text-xs text-gray-500">Chapter - 3</span>
                               Coordinate Geometry
                           </h2>
                           <div class="arrow-circle">></div>
@@ -137,7 +136,7 @@ include('./secondary-navbar.html');
                               <img src="assets/5.webp" alt="" class="w-full rounded-lg" />
                           </div>
                           <h2 class="w-2/3 font-ch-name">
-                              <span class="block text-sm other-ch">Chapter - 1</span>
+                              <span class="block text-xs text-gray-500">Chapter - 4</span>
                               Linear Equations in Two Variables
                           </h2>
                           <div class="arrow-circle">></div>
